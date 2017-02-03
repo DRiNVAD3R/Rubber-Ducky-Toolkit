022f8ef999ff9d0e7cfa0c59df909fe68c98fea7
DRiNVAD3R <david@mike-seyboth.de>
2017-02-03 17:02:19 +0100

    Initial Commit

---
+++ Rubber Ducky Toolkit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rubber Ducky Toolkit
+//
+//  Created by David Seyboth on 03/02/2017.
+//  Copyright © 2017 David Seyboth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
